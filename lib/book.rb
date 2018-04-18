0eecef7c71e454331ae7260bae97cbcc430f4783
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-18 15:57:30 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,10 +6,14 @@ GENRES = []
 
   def initialize(title)
     @title = title
-    GENRES << genre
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genres = genre
+    GENRES << genre
   end
 end
